2e4b50dbeaf67af406126a764b5ba9feffa22ef1
Alfaizi Ahmad Zahran <zahranalfaizi@gmail.com>
2025-02-05 23:22:41 +0700

    Menambahkan fitur keliling

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ print("Aplikasi Menghitung Luas Segitiga\n")
 def segitiga(alas,tinggi) :
     return (alas * tinggi)/2
 
+def keliling(s1,s2,a):
+    return s1+s2+a
+
 
 a = int(input("Masukan alas Segitiga :"))
 t = int(input("Masukan tinggi Segitiga :"))
+s1 = int(input("Masukan sisi 1 : "))
+s2 = int(input("Masukan sisi 2 : "))
 
 
-hasil = segitiga(a,t)
 
-print("Hasilnya adalah :",hasil)
+hasil1 = segitiga(a,t)
+hasil2 = keliling(s1,s2,a)
+
+
+print("Hasilnya adalah :",hasil1)
+print("Keliling : ",hasil2)
