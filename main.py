51bfc9a5e5ce7957e8d863a9c09d21ac420fdee9
Alfaizi Ahmad Zahran <zahranalfaizi@gmail.com>
2025-02-05 23:10:18 +0700

    Membuat Program segitiga sederhana

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print("Ini Sudah Masuk ke Github Guyss")
+
+
+print("Aplikasi Menghitung Luas Segitiga\n")
+
+def segitiga(alas,tinggi) :
+    return (alas * tinggi)/2
+
+
+a = int(input("Masukan alas Segitiga :"))
+t = int(input("Masukan tinggi Segitiga :"))
+
+
+hasil = segitiga(a,t)
+
+print("Hasilnya adalah :",hasil)
